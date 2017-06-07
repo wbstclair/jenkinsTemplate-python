826c570daa30890ed6d6fcb28828b94db67de8ce
wbstclair <ben@wbstclair.com>
2017-06-06 18:35:03 -0700

    Solved array partition problem

--- src/arrayPartition1.py
+++ src/arrayPartition1.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def ArrayPairSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        # your solution here
-
-        return 0
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		nums.sort()
+		tupleList = []
+		tupleSum = 0
+		for i in range(0,len(nums),2):
+			currentTuple = (nums[i], nums[i+1])
+			tupleList.append(currentTuple)
+			tupleSum += min(currentTuple)
+		return tupleSum
 
